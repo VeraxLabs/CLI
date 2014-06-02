4bab18bc7a4c080c043db91d432c1b01af55e7f8
Prashant Singhal <psl@veraxlabs.com>
2014-06-02 20:39:29 +0530

    Added logging support

--- vl/cli.py
+++ vl/cli.py
@@ -3,11 +3,18 @@ CLI Driver
 """
 
 import click
+import logging
+from logging.config import dictConfig
+from config.logging import get_config
 
 
 @click.command()
-def run():
+@click.option('--debug', is_flag=True)
+def run(debug):
     """
     Main entry point into the application
     """
+    dictConfig(get_config(debug))
+    logger = logging.getLogger(__name__)
+    logger.debug('Processing command line ...')
     click.echo('Welcome to VeraxLabs CLI')
